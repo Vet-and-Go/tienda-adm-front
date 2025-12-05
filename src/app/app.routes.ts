1e1569435fe56138c170db34da2b2b72ed93f403
aivan <aivanalbaranez@gmail.com>
2025-12-05 13:38:59 +0100

    haciendo front header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { App } from './components/paginas/app/app';
+import { Products } from './components/paginas/products/products';
+import { Categories } from './components/paginas/categorias/categories';
+import { Inicio } from './components/paginas/inicio/inicio';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:Inicio},
+    {path: 'products',component:Products},
+    {path: 'categories',component:Categories},
+    {path: '**', redirectTo: ''}
+    
+];
